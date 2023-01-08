a735c8a223262260a4576120139d3ea69321de79
mhrifai <mkhrifai99@gmail.com>
2023-01-08 08:58:01 +0700

    CRUD Absent

--- WebMvc/Controllers/AbsensiController.cs
+++ WebMvc/Controllers/AbsensiController.cs
@@ -11,8 +11,47 @@ namespace WebMvc.Controllers
         }
 
         private static List<AbsensiViewModel> _absensiViewModels = new List<AbsensiViewModel>()
-        {
-            new AbsensiViewModel(1, 002, '01/01/2023', '01/01/2023', "Bali", "Lupa"),
+        {            
+            new AbsensiViewModel(1, 002, new DateTime(2017, 8, 24, 08, 00, 0), new DateTime(2017, 8, 24, 17, 00, 0), "Bali", "Lupa"),
+            new AbsensiViewModel(2, 003, new DateTime(2017, 8, 24, 08, 00, 0), new DateTime(2017, 8, 24, 17, 00, 0), "Denpasar", "Lupa"),
         };
+
+        public IActionResult List()
+        {
+            return View(_absensiViewModels);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            AbsensiViewModel model = _absensiViewModels.Find(x=>x.Id.Equals(id));
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, [Bind("Id, EmployeeId, AbsenStartDate, AbsenEndDate, Location, Description")] AbsensiViewModel absensi)
+        {
+            AbsensiViewModel absensiOld = _absensiViewModels.Find(x=>x.Id.Equals(id));
+            _absensiViewModels.Remove(absensiOld);
+
+            _absensiViewModels.Add(absensi);
+            return Redirect("List");
+        }
+
+        public IActionResult Details(int id)
+        {
+            AbsensiViewModel absensi =(
+                from p in _absensiViewModels
+                where p.Id.Equals(id)
+                select p).SingleOrDefault(new AbsensiViewModel());
+            return View(absensi);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            AbsensiViewModel absensi = _absensiViewModels.Find(x => x.Id.Equals(id));
+            _absensiViewModels.Remove(absensi);
+            return Redirect("List");
+        }
     }
 }
--- WebMvc/Models/AbsensiViewModel.cs
+++ WebMvc/Models/AbsensiViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace WebMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMvc.Models
 {
     public class AbsensiViewModel
     {
         public int Id { get; set; }
         public int EmployeeId { get; set; }
+
         public DateTime AbsentStartDate { get; set; }
+
         public DateTime AbsentEndDate { get; set; }
         public String Location { get; set; }
         public String Description { get; set; }
